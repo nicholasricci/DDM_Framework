d1b1d9b8349e4117e631829e4062f4ef2341df0b
Nicholas <bururez@gmail.com>
2014-09-19 15:28:06 +0200

    Fix the make file for interval tree for bin folder

--- launcher.sh
+++ launcher.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
 echo "Launcher starts!"
-
-echo "Move to utils and clean"
+echo "Move to launcher folder"
 cd launcher
-make clean
-echo "Make makefile"
-make
+#Check first parameter if insterted, if "clean" string then use make clean command
+if [ ! -z "$1" ] && [ "$1" == "clean" ];
+then
+  make clean
+fi
+
+if [ ! -f ./bin/launcher ];
+then
+  echo "Clean junk"
+  make clean
+  echo "Compiling Makefile"
+  make
+fi
+echo "Execute launcher in c"
+
+if [ ! -z "$1" ] && [ "$1" != "clean" ];
+then
+  ./bin/launcher "$1"
+else
+  ./bin/launcher
+fi
--- launcher/src/launcher.c
+++ launcher/src/launcher.c
@@ -2,7 +2,20 @@
 #include "../inc/comparison.h"
 #include "../inc/launcher.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+
 int main(int argc, char *argv[]) {
 
+  printf("number parameters: %d\n", argc);
+  if ( argc != 2 ) /* argc should be 1 for correct execution */
+  {
+      /* We print argv[0] assuming it is the program name */
+      
+      printf("usage: %s filename\n", argv[0] );
+  }
+  else 
+      printf("Right number of parameters: %s\n", argv[1]);
+  
   return 0;
 }
