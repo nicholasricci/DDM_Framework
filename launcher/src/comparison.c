2b49fc0e86c7ff2ad33db4c0b1ddffda1fcf85d7
Nicholas <bururez@gmail.com>
2014-09-26 02:30:35 +0200

    fix function.sh and now all algorithms run are execute correctly, create an INSTRUCTION.txt text file and upgrade punto-tirocinio.txt

--- launcher/src/comparison.c
+++
@@ -1,5 +0,0 @@
-#include "../inc/comparison.h"
-
-int print_comparison(int argc, char *argv[]) {
-  return 2;
-} 
--- launcher/src/inputoutput.c
+++
@@ -1,5 +0,0 @@
-#include "../inc/inputoutput.h"
-
-int print_inputoutput(int argc, char *argv[]) {
-  return 1;
-}
